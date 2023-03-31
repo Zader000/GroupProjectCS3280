51b98f361d6e4be44a635f8f86b7231d6c3476d1
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-03-31 11:13:54 -0600

    finsihed clsItemsSQL class, now to comment clsItemsLogic class and update UI

---
+++ GroupProject/Items/clsItemsSQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Items
+{
+    public static class clsItemsSQL
+    {
+        public static string getItems()
+        {
+            return "SELECT ItemCode, ItemDesc, Cost FROM ItemDesc";
+        }
+
+        public static string getInvoiceNum(string ItemCode)
+        {
+            return $"SELECT distinct(InvoiceNum) FROM LineItems WHERE ItemCode = '{ItemCode}'";
+        }
+
+        public static string updateDesc(string ItemDesc, string Cost, string ItemCode)
+        {
+            return $"UPDATE ItemDesc SET ItemDesc = {ItemDesc}, COST = {Cost} WHERE ItemCode = '{ItemCode}'";
+        }
+
+        public static string insertDesc(string ItemCode, string ItemDesc, string Cost)
+        {
+            return $"INSERT INTO ItemDesc(ItemCode, ItemDesc, Cost) VALUES('{ItemCode}', '{ItemDesc}', {Cost})";
+        }
+
+        public static string deleteDesc(string ItemCode)
+        {
+            return $"DELETE FROM ItemDesc Where ItemCode = '{ItemCode}'";
+        }
+    }
+}
