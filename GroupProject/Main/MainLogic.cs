69c7adc4ceba1719d1c129296cc3a9fa652c228d
Tenzoh <122520241+Tenzoh@users.noreply.github.com>
2023-03-31 03:08:26 -0600

    Add files via upload

---
+++ GroupProject/Main/MainLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Main
+{
+    class MainLogic
+    {
+        //GetAllItems return List<clsItem>
+        //SaveNewInvoice(clsInvoice)
+        //EditInvoice(clsOldInvoice, clsNewInvoice)
+        //GetInvoice(InvoiceNumber) returns clsInvoice - Get the invoice and items for the selected invoice from search window
+    }
+}
