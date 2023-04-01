bfb437372d0661fe41327c44edb467e38a050186
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-04-01 13:39:10 -0600

    Finished up Items prototype.

--- GroupProject/Items/clsItemsSQL.cs
+++ GroupProject/Items/clsItemsSQL.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupProject.Items
 {
-    public static class clsItemsSQL
+    public class clsItemsSQL
     {
+        private string sConnectionString;
+        public clsItemsSQL()
+        {
+            sConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data source= " + Directory.GetCurrentDirectory() + "\\DB.accdb";
+        }
         public static string getItems()
         {
             return "SELECT ItemCode, ItemDesc, Cost FROM ItemDesc";
@@ -31,6 +40,34 @@ namespace GroupProject.Items
         public static string deleteDesc(string ItemCode)
         {
             return $"DELETE FROM ItemDesc Where ItemCode = '{ItemCode}'";
+        }
+
+        public DataSet ExecuteSQLStatement(string sSQL, ref int iRetVal)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                using (OleDbConnection conn = new OleDbConnection(sConnectionString))
+                {
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter())
+                    {
+                        conn.Open();
+
+                        adapter.SelectCommand = new OleDbCommand(sSQL, conn);
+                        adapter.SelectCommand.CommandTimeout = 0;
+
+                        adapter.Fill(ds);
+                    }
+                }
+
+                iRetVal = ds.Tables[0].Rows.Count;
+
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
+            }
         }
     }
 }
